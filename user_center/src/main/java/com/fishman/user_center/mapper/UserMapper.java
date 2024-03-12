e3366431f7504671228f165a44e3166dbd40098f
fishman <cy13692810010@163.com>
2024-03-13 01:05:50 +0800

    前端初始化，后端初始化并添加mybatis—puls测试

---
+++ user_center/src/main/java/com/fishman/user_center/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.fishman.user_center.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fishman.user_center.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
