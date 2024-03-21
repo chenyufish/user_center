40ad2b5b2cceff99b2ff367102a2be058844d8fa
fishman <cy13692810010@163.com>
2024-03-21 22:16:49 +0800

    更新全局异常处理

--- user-center/src/main/java/com/fishman/usercenter/constant/UserConstant.java
+++
@@ -1,9 +0,0 @@
-package com.fishman.usercenter.constant;
-
-public interface UserConstant {
-
-    String USER_LOGIN_STATE="userLoginState";//记录用户登录状态
-
-    int DEFAULT_ROLE=0;
-    int ADMIN_ROLE=1;
-}
